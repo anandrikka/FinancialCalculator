7f5a12d63057ae9abcdaf580a660ce866645508d
anandrikka <anand.rikka@outlook.com>
2016-05-11 00:45:21 +0530

    Initial commit

---
+++ app/src/main/java/com/wordpress/techanand/financialcalculator/app/Constants.java
@@ -0,0 +1,9 @@
+package com.wordpress.techanand.financialcalculator.app;
+
+/**
+ * Created by Anand Rikka on 5/11/2016.
+ */
+public class Constants {
+
+    public final static String LOG = "app_log";
+}
